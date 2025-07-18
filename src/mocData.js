5795e6bbed7cd3a2865e8503c46c25848364ebf4
heemin1285 <bonjovi0080@gmail.com>
2025-07-18 17:58:50 +0900

    0718

---
+++ src/mocData.js
@@ -0,0 +1,31 @@
+let data = [
+  {
+     id: 0,
+     title: 'apple',
+     content : '이것은 사과입니다.',
+     price : 12000,
+
+
+  },
+
+  {
+    id : 1,
+    title : 'strawberry',
+    content : '이것은 딸기',
+    price: 25000,
+
+  },
+
+   {
+    id : 2,
+    title : 'watermelon',
+    content : '이것은 수박',
+    price: 30000,
+
+  },
+
+
+
+]
+
+export default data;
